dfaeaa5ef411ad34023be8383362dc787d4101c4
Zack <zecharyw@gmail.com>
2022-04-17 23:25:09 +0800

    Add grpahCMSImageLoader

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import moment from 'moment';
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import moment from 'moment'
+import Link from 'next/link'
 
-import { grpahCMSImageLoader } from '../util';
-import { getSimilarPosts, getRecentPosts } from '../services';
+import { grpahCMSImageLoader } from '../util'
+import { getSimilarPosts, getRecentPosts } from '../services'
 
 const PostWidget = ({ categories, slug }) => {
-  const [relatedPosts, setRelatedPosts] = useState([]);
+  const [relatedPosts, setRelatedPosts] = useState([])
 
   useEffect(() => {
     if (slug) {
       getSimilarPosts(categories, slug).then((result) => {
-        setRelatedPosts(result);
-      });
+        setRelatedPosts(result)
+      })
     } else {
       getRecentPosts().then((result) => {
-        setRelatedPosts(result);
-      });
+        setRelatedPosts(result)
+      })
     }
-  }, [slug]);
+  }, [slug])
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
       {relatedPosts.map((post, index) => (
-        <div key={index} className="flex items-center w-full mb-4">
+        <div key={index} className="mb-4 flex w-full items-center">
           <div className="w-16 flex-none">
             <Image
               loader={grpahCMSImageLoader}
@@ -33,18 +35,27 @@ const PostWidget = ({ categories, slug }) => {
               height="60px"
               width="60px"
               unoptimized
-              className="align-middle rounded-full"
+              className="rounded-full align-middle"
               src={post.featuredImage.url}
             />
           </div>
-          <div className="flex-grow ml-4">
-            <p className="text-gray-500 font-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
-            <Link href={`/post/${post.slug}`} className="text-md" key={index} passHref>{post.title}</Link>
+          <div className="ml-4 flex-grow">
+            <p className="font-xs text-gray-500">
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              className="text-md"
+              key={index}
+              passHref
+            >
+              {post.title}
+            </Link>
           </div>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default PostWidget;
+export default PostWidget
